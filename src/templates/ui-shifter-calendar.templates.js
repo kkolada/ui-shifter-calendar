25edb1f55fdd8f1c6e67723e870432e1de11d74d
Krzysztof Kolada <krzysztof.kolada@gmail.com>
2017-10-03 13:57:02 +0200

    Template changed.

--- src/templates/ui-shifter-calendar.templates.js
+++ src/templates/ui-shifter-calendar.templates.js
@@ -3,5 +3,5 @@ angular.module('UI.Shifter.Calendar', ['src/templates/ui-shifter-calendar.tpl.ht
 angular.module('src/templates/ui-shifter-calendar.tpl.html', []).run(['$templateCache', function($templateCache) {
   'use strict';
   $templateCache.put('src/templates/ui-shifter-calendar.tpl.html',
-    '<div>Template test.</div>');
+    '<div>UI.Shifter.Calendar plugin, test template.</div>');
 }]);

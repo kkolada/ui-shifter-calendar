58e3c07f3da6f0c2fc1b4d4d45d40fe13cfdb77a
Krzysztof Kolada <krzysztof.kolada@gmail.com>
2017-10-05 12:28:00 +0200

    Showcase for days filter - working days/weekend.

--- showcase/showcase.js
+++ showcase/showcase.js
@@ -7,7 +7,10 @@ shifterCalendarShowcaseApp.controller('ShowcaseCtrl', function($scope) {
 
     $scope.momentJsDate = moment().format('MMMM Do YYYY, HH:mm:ss');
     $scope.data = {
-        availableHours: [{t:0}, {t:1}, {t:2}, {t:3}, {t:4}, {t:5}, {t:6}, {t:7}, {t:8}, {t:9}, {t:10}, {t:11}, {t:12}, {t:13}, {t:14}, {t:15}, {t:16}, {t:17}, {t:18}, {t:19}, {t:20}, {t:21}, {t:22}, {t:23}, {t:24}],
+        availableHours: [
+            {t:0}, {t:1}, {t:2}, {t:3}, {t:4}, {t:5}, {t:6}, {t:7}, {t:8}, {t:9}, {t:10}, {t:11}, {t:12},
+            {t:13}, {t:14}, {t:15}, {t:16}, {t:17}, {t:18}, {t:19}, {t:20}, {t:21}, {t:22}, {t:23}, {t:24}
+            ],
         startTime: {t:0},
         endTime: {t:1}
     };
@@ -19,6 +22,24 @@ shifterCalendarShowcaseApp.controller('ShowcaseCtrl', function($scope) {
         fri: true,
         sat: true,
         sun: true
+    };
+    $scope.activateWorkingDays = function() {
+        $scope.checkboxModel.mon = true;
+        $scope.checkboxModel.tue = true;
+        $scope.checkboxModel.wed = true;
+        $scope.checkboxModel.thu = true;
+        $scope.checkboxModel.fri = true;
+        $scope.checkboxModel.sat = false;
+        $scope.checkboxModel.sun = false;
+    };
+    $scope.activateWeekend = function() {
+        $scope.checkboxModel.mon = false;
+        $scope.checkboxModel.tue = false;
+        $scope.checkboxModel.wed = false;
+        $scope.checkboxModel.thu = false;
+        $scope.checkboxModel.fri = false;
+        $scope.checkboxModel.sat = true;
+        $scope.checkboxModel.sun = true;
     }
 
 });

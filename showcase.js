5e2f776b5a06ebb28560186ecd4e76679d82e651
Krzysztof Kolada <krzysztof.kolada@gmail.com>
2017-10-16 20:28:44 +0200

    GitHub page - showcase.

---
+++ showcase.js
@@ -0,0 +1,168 @@
+/**
+ * UI Shifter Calendar Showcase
+ */
+var shifterCalendarShowcaseApp = angular.module('shifterCalendarShowcaseApp', ['UI.Shifter.Calendar']);
+
+shifterCalendarShowcaseApp.controller('ShowcaseCtrl', function($scope) {
+
+    $scope.momentJsDate = moment().format('MMMM Do YYYY, HH:mm:ss');
+    $scope.data = {
+        availableHours: [
+            {t:0}, {t:1}, {t:2}, {t:3}, {t:4}, {t:5}, {t:6}, {t:7}, {t:8}, {t:9}, {t:10}, {t:11}, {t:12},
+            {t:13}, {t:14}, {t:15}, {t:16}, {t:17}, {t:18}, {t:19}, {t:20}, {t:21}, {t:22}, {t:23}, {t:24}
+            ],
+        startTime: {t:0},
+        endTime: {t:15},
+        availableDays: [
+            {d:'Monday'}, {d:'Tuesday'}, {d:'Wednesday'}, {d:'Thursday'}, {d:'Friday'}, {d:'Saturday'}, {d:'Sunday'}
+        ],
+        daySelected: {d:'Monday'}
+    };
+    $scope.checkboxModel = {
+        mon: true,
+        tue: true,
+        wed: true,
+        thu: true,
+        fri: true,
+        sat: true,
+        sun: true
+    };
+    $scope.activateWorkingDays = function () {
+        $scope.checkboxModel.mon = true;
+        $scope.checkboxModel.tue = true;
+        $scope.checkboxModel.wed = true;
+        $scope.checkboxModel.thu = true;
+        $scope.checkboxModel.fri = true;
+        $scope.checkboxModel.sat = false;
+        $scope.checkboxModel.sun = false;
+    };
+    $scope.activateWeekend = function () {
+        $scope.checkboxModel.mon = false;
+        $scope.checkboxModel.tue = false;
+        $scope.checkboxModel.wed = false;
+        $scope.checkboxModel.thu = false;
+        $scope.checkboxModel.fri = false;
+        $scope.checkboxModel.sat = true;
+        $scope.checkboxModel.sun = true;
+    };
+    $scope.events = [
+        {
+            day: 'Monday',
+            from: '03:00',
+            to: '05:00',
+            fraction: '2/4'
+        },
+        {
+            day: 'Tuesday',
+            from: '10:00',
+            to: '11:45',
+            fraction: '1/2'
+        },
+        {
+            day: 'Tuesday',
+            from: '05:20',
+            to: '10:45',
+            fraction: '3/4'
+        },
+        {
+            day: 'Monday',
+            from: '05:10',
+            to: '07:45',
+            fraction: '1/4'
+        },
+        {
+            day: 'Wednesday',
+            from: '07:13',
+            to: '07:28',
+            fraction: '5/8'
+        },
+        {
+            day: 'Monday',
+            from: '05:00',
+            to: '06:30',
+            fraction: '1/4'
+        },
+        {
+            day: 'Friday',
+            from: '02:00',
+            to: '03:15',
+            fraction: '7/8'
+        },
+        {
+            day: 'Monday',
+            from: '03:15',
+            to: '7:00',
+            fraction: '1/4'
+        },
+        {
+            day: 'Sunday',
+            from: '04:55',
+            to: '11:45',
+            fraction: '2/8'
+        },
+        {
+            day: 'Monday',
+            from: '05:00',
+            to: '09:00',
+            fraction: '1/4'
+        },
+        {
+            day: 'Thursday',
+            from: '07:00',
+            to: '07:15',
+            fraction: '3/8'
+        },
+        {
+            day: 'Monday',
+            from: '08:30',
+            to: '10:00',
+            fraction: '3/4'
+        },
+        {
+            day: 'Monday',
+            from: '02:00',
+            to: '03:00',
+            fraction: '2/3'
+        },
+        {
+            day: 'Monday',
+            from: '09:00',
+            to: '11:00',
+            fraction: '1/4'
+        },
+        {
+            day: 'Saturday',
+            from: '03:00',
+            to: '04:00',
+            fraction: '1/3'
+        },
+        {
+            day: 'Saturday',
+            from: '05:00',
+            to: '06:00',
+            fraction: '1/3'
+        },
+        {
+            day: 'Saturday',
+            from: '04:00',
+            to: '05:00',
+            fraction: '1/4'
+        },
+        {
+            day: 'Saturday',
+            from: '03:00',
+            to: '06:00',
+            fraction: '2/3'
+        }
+    ];
+    $scope.addNewEvent = function() {
+        $scope.events.push({
+            day: $scope.data.daySelected.d,
+            from: $scope.eventStartTime,
+            to: $scope.eventEndTime,
+            fraction: $scope.eventFraction
+        });
+    };
+
+});
+/* EOF */
